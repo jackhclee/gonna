10b6def55c61ffe3bf618a82976a69cc9dcfea39
jackhclee <38123458+jackhclee@users.noreply.github.com>
2023-02-13 00:50:56 +0000

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const port = process.env.PORT || 8080;
 const dotenv = require("dotenv").config()
 
 app.get("/", function(req, res) {
-    res.send("Hello World" + new Date() + process.env.GITHUB_SHA);
+const { Client } = require('pg')
+const client = new Client()
+await client.connect()
+ 
+const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+console.log(res.rows[0].message) // Hello world!
+await client.end()
+
+
+    res.send(res.rows[0].message + new Date() + process.env.GITHUB_SHA);
   }
 )
 
